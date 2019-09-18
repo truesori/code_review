f56c22cb299a764290fadecb476c24f243a54898
truesori <55432338+truesori@users.noreply.github.com>
2019-09-18 15:19:27 +0900

    Remove main function
    main 함수를 review comment에 따라서 제거하였습니다.

--- priority_queue_test.cc
+++ priority_queue_test.cc
@@ -50,7 +50,9 @@ TEST(PriorityQueueTest,Trivial){
 	
 }
 
+/* Remove main fuctino in test file
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+(/
